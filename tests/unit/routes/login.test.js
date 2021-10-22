f902096d8272b07d283a8454821fdd41172bec06
natourmazen <natourmazen@gmail.com>
2021-10-22 17:43:30 +0200

    Fix Login Test

--- tests/unit/routes/login.test.js
+++ tests/unit/routes/login.test.js
@@ -3,20 +3,23 @@ const bcrypt = require("bcrypt");
 const app = require("../../../startup/app");
 const { User } = require("../../../models/user");
 
-let user1 = {
+let user1 = new User({
   email: "example@example.com",
   password: "12345678",
-};
+  name: "example"
+});
 
 const user1_1 = {
   email: "example@example.com",
-  password: "12345678",
+  password: "12345678"
 };
 
 const user2 = {
   email: "example@example.com",
-  password: "1234567890",
+  password: "123456789"
 };
+
+
 
 async function encryptPassword(user) {
   const salt = await bcrypt.genSalt(10);
@@ -25,23 +28,27 @@ async function encryptPassword(user) {
 }
 
 describe("POST /api/login ", () => {
+  it('should return an error if the request body is invalid', async () => {
+    const response = await request(app).post("/api/login").send({});
+    expect(response.statusCode).toBe(400);
+  });
+  
   it("should return an error if email does not exist in the database", async () => {
     User.findOne = jest.fn();
-    const response = await request(app).post("/api/login").send(user1);
+    const response = await request(app).post("/api/login").send(user1_1);
     expect(response.statusCode).toBe(400);
   });
 
   it("should return an error if password does not exist in the database", async () => {
-    User.findOne = jest.fn(() => user1);
+    User.findOne = jest.fn().mockResolvedValue(user1);
     const response = await request(app).post("/api/login").send(user2);
     expect(response.statusCode).toBe(400);
   });
 
   it("should pass if email and password are valid", async () => {
     user1 = await encryptPassword(user1);
-    User.findOne = jest.fn(() => user1);
+    User.findOne = jest.fn().mockResolvedValue(user1);
     const response = await request(app).post("/api/login").send(user1_1);
-    console.log(response.body);
     expect(response.statusCode).toBe(200);
   });
 });
