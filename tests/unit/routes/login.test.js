b1108f5b91209b7f5f3873a16bcc214c51e1d8b8
Ahmad Ghalayini <ahmad2000gh@gmail.com>
2021-10-22 19:34:53 +0300

    Add Unit Tests

--- tests/unit/routes/login.test.js
+++ tests/unit/routes/login.test.js
@@ -4,22 +4,20 @@ const app = require("../../../startup/app");
 const { User } = require("../../../models/user");
 
 let user1 = new User({
+  name: "example",
   email: "example@example.com",
   password: "12345678",
-  name: "example"
 });
-
-const user1_1 = {
-  email: "example@example.com",
-  password: "12345678"
-};
 
 const user2 = {
   email: "example@example.com",
-  password: "123456789"
+  password: "12345678",
 };
 
-
+const user3 = {
+  email: "example@example.com",
+  password: "123456789",
+};
 
 async function encryptPassword(user) {
   const salt = await bcrypt.genSalt(10);
@@ -28,27 +26,27 @@ async function encryptPassword(user) {
 }
 
 describe("POST /api/login ", () => {
-  it('should return an error if the request body is invalid', async () => {
+  it("should return an error if the request body is invalid", async () => {
     const response = await request(app).post("/api/login").send({});
     expect(response.statusCode).toBe(400);
   });
-  
+
   it("should return an error if email does not exist in the database", async () => {
     User.findOne = jest.fn();
-    const response = await request(app).post("/api/login").send(user1_1);
+    const response = await request(app).post("/api/login").send(user2);
     expect(response.statusCode).toBe(400);
   });
 
   it("should return an error if password does not exist in the database", async () => {
     User.findOne = jest.fn().mockResolvedValue(user1);
-    const response = await request(app).post("/api/login").send(user2);
+    const response = await request(app).post("/api/login").send(user3);
     expect(response.statusCode).toBe(400);
   });
 
   it("should pass if email and password are valid", async () => {
     user1 = await encryptPassword(user1);
     User.findOne = jest.fn().mockResolvedValue(user1);
-    const response = await request(app).post("/api/login").send(user1_1);
+    const response = await request(app).post("/api/login").send(user2);
     expect(response.statusCode).toBe(200);
   });
 });
---
+++ tests/unit/routes/users.test.js
@@ -0,0 +1,58 @@
+const request = require("supertest");
+const bcrypt = require("bcrypt");
+const app = require("../../../startup/app");
+const { User } = require("../../../models/user");
+
+let user1 = new User({
+  name: "example",
+  email: "example@example.com",
+  phoneNumber: "01234567",
+  password: "12345678",
+  isShopOwner: true,
+});
+
+let user2 = {
+  name: "example",
+  email: "example@example.com",
+  phoneNumber: "01234567",
+  password: "12345678",
+  isShopOwner: false,
+};
+
+let user3 = {
+  name: "example",
+  email: "example1@example.com",
+  password: "12345678",
+  isShopOwner: true,
+};
+
+describe("POST /api/users ", () => {
+  it("should return an error if the request body is invalid", async () => {
+    const response = await request(app).post("/api/users").send({});
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return an error if email is already registered", async () => {
+    User.findOne = jest.fn(() => user1);
+    const response = await request(app).post("/api/users").send(user2);
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return an error if a shop owner is already registered and the new request also has shop owner true", async () => {
+    User.findOne = jest.fn(() => user1);
+    const response = await request(app).post("/api/users").send(user3);
+    expect(response.statusCode).toBe(403);
+  });
+
+  it("should pass if the request is valid", async () => {
+    User.findOne = jest.fn();
+    const response = await request(app).post("/api/users").send(user2);
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("should pass if the request is valid", async () => {
+    User.findOne = jest.fn();
+    const response = await request(app).post("/api/users").send(user3);
+    expect(response.statusCode).toBe(200);
+  });
+});
